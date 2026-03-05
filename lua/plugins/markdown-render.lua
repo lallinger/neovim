af6d9740a48fff1a386f6a0a056235b8cdb2dfac
Patrick Lallinger <patrick@lallinger.tech>
2026-03-05 21:17:33 +0000

    update renamed plugin, gitignore lazy-lock.json

--- lua/plugins/markdown-render.lua
+++ lua/plugins/markdown-render.lua
@@ -1,6 +1,9 @@
 return {
   "MeanderingProgrammer/render-markdown.nvim",
-  dependencies = { "nvim-treesitter/nvim-treesitter", "echasnovski/mini.icons" },
+  dependencies = {
+    "nvim-treesitter/nvim-treesitter",
+    "nvim-mini/mini.icons",
+  },
   ft = { "markdown", "norg", "rmd", "org" }, -- Required to trigger lazy loading in LazyVim
   ---@module "render-markdown"
   ---@type render.md.UserConfig
